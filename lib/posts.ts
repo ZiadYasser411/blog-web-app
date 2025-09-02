dd144fa600fadfdc681a39d4f5bc38f3533ed851
ZiadYasser411 <ziadyasser4111@gmail.com>
2025-09-02 22:40:49 +0300

    feat: created CRUD operations for Post

---
+++ lib/posts.ts
@@ -0,0 +1,110 @@
+"user-server";
+import prisma from "@/lib/prisma";
+
+function generateSlug(title: string): string {
+  return title
+    .toLowerCase()
+    .replace(/[^a-z0-9\s]/g, "")
+    .replace(/\s+/g, "-")
+    .trim();
+}
+export async function getPosts() {
+  try {
+    const posts = await prisma.post.findMany({
+      include: {
+        author: {
+          select: {
+            id: true,
+            name: true,
+            username: true,
+          },
+        },
+        comments: {
+          include: {
+            commenter: {
+              select: {
+                id: true,
+                name: true,
+                username: true,
+              },
+            },
+          },
+          orderBy: {
+            createdAt: "desc",
+          },
+        },
+        _count: {
+          select: { comments: true },
+        },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    return posts;
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    throw new Error("Failed to fetch posts");
+  }
+}
+
+export async function createPost(
+  title: string,
+  content: string,
+  authorId: string,
+  tags: string[]
+) {
+  if (!title || !authorId) {
+    throw new Error("Title and authorId are required");
+  }
+  try {
+    const slug = generateSlug(title);
+    const post = await prisma.post.create({
+      data: {
+        title,
+        content,
+        slug,
+        authorId,
+        tags,
+      },
+    });
+    return post;
+  } catch (error) {
+    console.error("Error creating post:", error);
+    throw new Error("Failed to create post");
+  }
+}
+
+export async function deletePost(id: string) {
+  try {
+    await prisma.post.delete({
+      where: {
+        id,
+      },
+    });
+    return true;
+  } catch (error) {
+    console.error("Error deleting post:", error);
+    throw new Error("Failed to delete post");
+  }
+}
+
+export async function editPost(id: string, title: string, content: string) {
+  try {
+    const slug = generateSlug(title);
+    await prisma.post.update({
+      where: {
+        id,
+      },
+      data: {
+        title,
+        content,
+        slug,
+      },
+    });
+    return true;
+  } catch (error) {
+    console.error("Error editing post:", error);
+    throw new Error("Failed to edit post");
+  }
+}

876e7342768f49031ef6821b9dac0cf4bb83e981
ZiadYasser411 <ziadyasser4111@gmail.com>
2025-09-06 23:24:50 +0300

    feat: added toggleLike action

--- app/post/actions.ts
+++ app/post/actions.ts
@@ -1,10 +1,11 @@
 "use server";
 import { Tag } from "@prisma/client";
-import { createPost } from "@/lib/service/postService";
+import { createPost, toggleLike } from "@/lib/service/postService";
 import { requireSession } from "@/lib/auth/require-session";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { TAG_OPTIONS } from "@/lib/constants/tags";
+import { createComment } from "@/lib/service/commentService";
 
 const allowedTags = new Set(TAG_OPTIONS);
 
@@ -20,3 +21,22 @@ export async function handleCreatePost(formdata: FormData) {
       redirect(`/post/${post.slug}`);
     }
   }
+
+  export async function handleCreateComment(formdata: FormData) {
+    const session = await requireSession();
+    const commenterId = session.user.id;
+    const postId = formdata.get("postId") as string;
+    const slug = formdata.get("slug") as string;
+    const content = formdata.get("content") as string;
+    if (postId && content) {
+      await createComment(postId, content, commenterId);
+      revalidatePath(`/post/${slug}`);
+    }
+  }
+
+  export async function toggleLikeAction(postId: string, slug?: string) {
+  const { user } = await requireSession();
+  const res = await toggleLike(postId, user.id);
+  if (slug) revalidatePath(`/post/${slug}`);
+  return res;
+}

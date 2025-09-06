dadd09bbe6e8ade6e164a483a906d358bf5c7f86
ZiadYasser411 <ziadyasser4111@gmail.com>
2025-09-06 20:41:19 +0300

    refactor: moved userList to admin page temporarily

---
+++ app/admin/page.tsx
@@ -0,0 +1,35 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { getAllUsers } from "@/lib/service/userService";
+import Link from "next/link";
+
+export default async function AdminHome() {
+  const users = await getAllUsers();
+  return (
+    <div className="flex flex-col md:flex-row gap-8 p-12">
+      <div className="flex-2">
+        <h1 className="text-2xl font-bold mb-6">Users</h1>
+        <ul className="space-y-4 rounded-lg border p-6 shadow-sm">
+          {users.map((user) => (
+            <li key={user.id} className="flex items-center-safe">
+              <div className="pr-4">
+                <Avatar>
+                  <AvatarFallback> {user.firstName.substring(0, 1).toUpperCase()}{user.lastName.substring(0, 1).toUpperCase()}</AvatarFallback>
+                </Avatar>
+              </div>
+              <div className="">
+                <Link href={`/users/${user.id}`}>
+                  <h2 className="text-lg font-bold">{user.firstName} {user.lastName}</h2>
+                </Link>
+                <p className="text-xs text-gray-400">{user.email}</p>
+                <div className="flex">
+                  <p className="text-xs text-gray-400 pr-2">posts: {user._count.posts}</p>
+                  <p className="text-xs text-gray-400">comments: {user._count.comments}</p>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,20 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { getAllUsers } from "@/lib/service/userService";
-import Link from "next/link";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth/options";
+import { redirect } from "next/navigation";
+import LoginForm from "./login/LoginForm";
+export default async function Home({ searchParams }: {
+  searchParams?: { callbackUrl?: string };
+})
+{
+  const session = await getServerSession(authOptions);
+  const callbackUrl = searchParams?.callbackUrl || "/post";
+  if (session?.user?.id) redirect(callbackUrl);
 
-export default async function Home() {
-  const users = await getAllUsers();
   return (
-    <div className="flex flex-col md:flex-row gap-8 p-12">
-      <div className="flex-2">
-        <h1 className="text-2xl font-bold mb-6">Users</h1>
-        <ul className="space-y-4 rounded-lg border p-6 shadow-sm">
-          {users.map((user) => (
-            <li key={user.id} className="flex items-center-safe">
-              <div className="pr-4">
-                <Avatar>
-                  <AvatarFallback> {user.firstName.substring(0, 1).toUpperCase()}{user.lastName.substring(0, 1).toUpperCase()}</AvatarFallback>
-                </Avatar>
-              </div>
-              <div className="">
-                <Link href={`/users/${user.id}`}>
-                  <h2 className="text-lg font-bold">{user.firstName} {user.lastName}</h2>
-                </Link>
-                <p className="text-xs text-gray-400">{user.email}</p>
-                <div className="flex">
-                  <p className="text-xs text-gray-400 pr-2">posts: {user._count.posts}</p>
-                  <p className="text-xs text-gray-400">comments: {user._count.comments}</p>
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
+    <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm callbackUrl={callbackUrl}/>
       </div>
     </div>
-  );
+  )
 }

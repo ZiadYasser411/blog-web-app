3aa03fe189d0ec93df17dd4b1cc9fc257c76b5af
ZiadYasser411 <ziadyasser4111@gmail.com>
2025-09-06 04:22:01 +0300

    fix: implemented session in login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,20 @@
-import { LoginForm } from "@/components/LoginForm"
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth/options";
+import { redirect } from "next/navigation";
+import LoginForm from "./LoginForm";
 
-export default function LoginPage() {
+export default async function LoginPage({ searchParams }: {
+  searchParams?: { callbackUrl?: string };
+})
+{
+  const session = await getServerSession(authOptions);
+  const callbackUrl = searchParams?.callbackUrl || "/";
+  if (session?.user?.id) redirect(callbackUrl);
+
   return (
     <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <LoginForm />
+        <LoginForm callbackUrl={callbackUrl}/>
       </div>
     </div>
   )
